e495510be8861854e733b80d2d366f23a856bf7b
Amarjit-Gupta <168736859+Amarjit-Gupta@users.noreply.github.com>
2025-11-22 11:18:28 +0530

    first commit
    Increase height of the chart container for better visibility.

--- src/components/BarChartComponent.jsx
+++ src/components/BarChartComponent.jsx
@@ -25,7 +25,7 @@ const BarChartComponent = ({ data }) => {
   return (
     <div className="bg-white p-5 rounded-xl border w-full min-w-0 flex flex-col  shadow-sm hover:shadow-lg border-gray-200 transition-shadow duration-500">
       <h2 className="text-lg font-semibold mb-3">Channel Performance</h2>
-      <div className="w-full min-w-0  h-50 md:h-80 overflow-hidden">
+      <div className="w-full min-w-0 h-60 md:h-80 overflow-hidden">
         <ResponsiveContainer width="100%" height={chartHeight}>
           <BarChart data={data}>
             <CartesianGrid strokeDasharray="3 3" />
